4582da05e055a78002670f5dd73432f8d0bf42d4
L1nSn0w <l1nsn0w@qq.com>
2025-09-10 22:10:07 +0800

    完成搜索插入位置和二分查找函数的初步实现

---
+++ 35.搜索插入位置.go
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=golang
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+func searchInsert(nums []int, target int) int {
+    left, right := 0, len(nums)
+	
+	for left <= right 
+}
+// @lc code=end
+
