af756995c96044e7e0b937a5b0a12460105d27a5
L1nSn0w <l1nsn0w@qq.com>
2025-09-15 11:09:47 +0800

    添加反转链表算法的初始实现，包含函数 reverseList

---
+++ 206.反转链表.go
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=golang
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode.cn/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (76.22%)
+ * Likes:    3966
+ * Dislikes: 0
+ * Total Accepted:    2.5M
+ * Total Submissions: 3.3M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：head = []
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000
+ *
+ *
+ *
+ *
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ *
+ *
+ *
+ */
+
+package main
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	return help(nil, head)
+}
+
+func help(pre, head*ListNode) *ListNode {
+		if head == nil {
+			return pre
+		}
+
+		next := head.Next
+		head.Next = pre
+		
+		return help(head, next)
+	}
+// @lc code=end
+
