3b23ba3c332c56dd0c6fe7255d2af9b945fa803a
L1nSn0w <l1nsn0w@qq.com>
2025-11-16 16:18:58 +0800

    完善填充每个节点的下一个右侧节点指针 II 的实现，优化了队列处理逻辑

--- 117.填充每个节点的下一个右侧节点指针-ii.go
+++ 117.填充每个节点的下一个右侧节点指针-ii.go
@@ -14,56 +14,60 @@
  * Testcase Example:  '[1,2,3,4,5,null,7]'
  *
  * 给定一个二叉树：
- * 
- * 
+ *
+ *
  * struct Node {
  * ⁠ int val;
  * ⁠ Node *left;
  * ⁠ Node *right;
  * ⁠ Node *next;
  * }
- * 
+ *
  * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL 。
- * 
+ *
  * 初始状态下，所有 next 指针都被设置为 NULL 。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：root = [1,2,3,4,5,null,7]
  * 输出：[1,#,2,3,#,4,5,7,#]
  * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。序列化输出按层序遍历顺序（由 next
  * 指针连接），'#' 表示每层的末尾。
- * 
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：root = []
  * 输出：[]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 树中的节点数在范围 [0, 6000] 内
  * -100 <= Node.val <= 100
- * 
- * 
+ *
+ *
  * 进阶：
- * 
- * 
+ *
+ *
  * 你只能使用常量级额外空间。
  * 使用递归解题也符合要求，本题中递归程序的隐式栈空间不计入额外空间复杂度。
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
  */
+
+package main
+
+import "container/list"
 
 // @lc code=start
 /**
@@ -77,7 +81,33 @@
  */
 
 func connect(root *Node) *Node {
-	
+    if root == nil { //防止为空
+        return root
+    }
+    queue := list.New()
+    queue.PushBack(root)
+    tmpArr := make([]*Node, 0)
+    for queue.Len() > 0 {
+        length := queue.Len() //保存当前层的长度，然后处理当前层（十分重要，防止添加下层元素影响判断层中元素的个数）
+        for i := 0; i < length; i++ {
+            node := queue.Remove(queue.Front()).(*Node) //出队列
+            if node.Left != nil {
+                queue.PushBack(node.Left)
+            }
+            if node.Right != nil {
+                queue.PushBack(node.Right)
+            }
+            tmpArr = append(tmpArr, node) //将值加入本层切片中
+        }
+        if len(tmpArr) > 1 {
+            // 遍历每层元素,指定next
+            for i := 0; i < len(tmpArr)-1; i++ {
+                tmpArr[i].Next = tmpArr[i+1]
+            }
+        }
+        tmpArr = []*Node{} //清空层的数据
+    }
+    return root
 }
 // @lc code=end
 
