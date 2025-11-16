4ada121e677f1e5d7a9e1ef26007cb17d3f5dfd8
L1nSn0w <l1nsn0w@qq.com>
2025-11-16 18:14:07 +0800

    添加平衡二叉树的完整实现，包含函数 isBalanced 和辅助函数 getHeight

---
+++ solved/110.平衡二叉树.go
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=golang
+ *
+ * [110] 平衡二叉树
+ *
+ * https://leetcode.cn/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (59.63%)
+ * Likes:    1650
+ * Dislikes: 0
+ * Total Accepted:    754.7K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，判断它是否是 平衡二叉树
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = []
+ * 输出：true
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中的节点数在范围 [0, 5000] 内
+ * -10^4 <= Node.val <= 10^4
+ *
+ *
+ */
+
+package main
+
+// 返回以该节点为根节点的二叉树的高度，如果不是平衡二叉树了则返回-1
+func getHeight(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    l, r := getHeight(root.Left), getHeight(root.Right)
+    if l == -1 || r == -1 {
+        return -1
+    }
+    if l - r > 1 || r - l > 1 {
+        return -1
+    }
+    return max(l, r) + 1
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isBalanced(root *TreeNode) bool {
+    h := getHeight(root)
+    if h == -1 {
+        return false
+    }
+    return true
+}
+// @lc code=end
+
