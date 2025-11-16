1de8e2666fe6ae3105e4f4e68f624ef361357dea
L1nSn0w <l1nsn0w@qq.com>
2025-11-16 16:01:07 +0800

    添加在每个树行中找最大值的算法，包含函数 largestValues

---
+++ solved/515.在每个树行中找最大值.go
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=515 lang=golang
+ *
+ * [515] 在每个树行中找最大值
+ *
+ * https://leetcode.cn/problems/find-largest-value-in-each-tree-row/description/
+ *
+ * algorithms
+ * Medium (66.76%)
+ * Likes:    406
+ * Dislikes: 0
+ * Total Accepted:    194.4K
+ * Total Submissions: 291.4K
+ * Testcase Example:  '[1,3,2,5,3,null,9]'
+ *
+ * 给定一棵二叉树的根节点 root ，请找出该二叉树中每一层的最大值。
+ *
+ *
+ *
+ * 示例1：
+ *
+ *
+ *
+ *
+ * 输入: root = [1,3,2,5,3,null,9]
+ * 输出: [1,3,9]
+ *
+ *
+ * 示例2：
+ *
+ *
+ * 输入: root = [1,2,3]
+ * 输出: [1,3]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 二叉树的节点个数的范围是 [0,10^4]
+ * -2^31 <= Node.val <= 2^31 - 1
+ *
+ *
+ *
+ *
+ */
+
+package main
+
+import (
+	"container/list"
+	"math"
+)
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func largestValues(root *TreeNode) []int {
+    if root == nil {
+        //防止为空
+        return nil
+    }
+    queue := list.New()
+    queue.PushBack(root)
+    ans := make([]int, 0)
+    temp := math.MinInt64
+    // 层序遍历
+    for queue.Len() > 0 {
+        //保存当前层的长度，然后处理当前层（十分重要，防止添加下层元素影响判断层中元素的个数）
+        length := queue.Len()
+        for i := 0; i < length; i++ {
+            node := queue.Remove(queue.Front()).(*TreeNode)//出队列
+            // 比较当前层中的最大值和新遍历的元素大小，取两者中大值
+            temp = max(temp, node.Val)
+            if node.Left != nil {
+                queue.PushBack(node.Left)
+            }
+            if node.Right != nil {
+                queue.PushBack(node.Right)
+            }
+        }
+        ans = append(ans, temp)
+        temp = math.MinInt64
+    }
+    return ans
+}
+
+func max(x, y int) int {
+    if x > y {
+        return x
+    }
+    return y
+}
+// @lc code=end
+
