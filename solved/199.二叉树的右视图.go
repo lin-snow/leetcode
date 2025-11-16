a9a16d2b16a6523665f0a23fbce72ab5fec1d1d9
L1nSn0w <l1nsn0w@qq.com>
2025-11-16 14:23:45 +0800

    添加二叉树的右视图算法，包含函数 rightSideView

---
+++ solved/199.二叉树的右视图.go
@@ -0,0 +1,111 @@
+/*
+ * @lc app=leetcode.cn id=199 lang=golang
+ *
+ * [199] 二叉树的右视图
+ *
+ * https://leetcode.cn/problems/binary-tree-right-side-view/description/
+ *
+ * algorithms
+ * Medium (71.94%)
+ * Likes:    1256
+ * Dislikes: 0
+ * Total Accepted:    721.8K
+ * Total Submissions: 996.5K
+ * Testcase Example:  '[1,2,3,null,5,null,4]'
+ *
+ * 给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,2,3,null,5,null,4]
+ *
+ * 输出：[1,3,4]
+ *
+ * 解释：
+ *
+ *
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,2,3,4,null,null,null,5]
+ *
+ * 输出：[1,3,4,5]
+ *
+ * 解释：
+ *
+ *
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1,null,3]
+ *
+ * 输出：[1,3]
+ *
+ *
+ * 示例 4：
+ *
+ *
+ * 输入：root = []
+ *
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 二叉树的节点个数的范围是 [0,100]
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
+
+package main
+
+import "container/list"
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rightSideView(root *TreeNode) []int {
+    if root == nil {
+        return nil
+    }
+    res := make([]int, 0)
+    queue := list.New()
+    queue.PushBack(root)
+
+    for queue.Len() > 0 {
+        length := queue.Len()
+        for i := 0; i < length; i++ {
+            node := queue.Remove(queue.Front()).(*TreeNode)
+            if node.Left != nil {
+                queue.PushBack(node.Left)
+            }
+            if node.Right != nil {
+                queue.PushBack(node.Right)
+            }
+            // 取每层的最后一个元素，添加到结果集中
+            if i == length-1 {
+                res = append(res, node.Val)
+            }
+        }
+    }
+    return res
+}
+// @lc code=end
+
