2f4ac6d022c284643b3929583d57f751aea95f83
L1nSn0w <l1nsn0w@qq.com>
2025-11-16 16:23:32 +0800

    添加二叉树最大深度的完整实现，包含函数 maxDepth

---
+++ solved/104.二叉树的最大深度.go
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=golang
+ *
+ * [104] 二叉树的最大深度
+ *
+ * https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (78.84%)
+ * Likes:    2036
+ * Dislikes: 0
+ * Total Accepted:    1.8M
+ * Total Submissions: 2.3M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树 root ，返回其最大深度。
+ *
+ * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ *
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：3
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,null,2]
+ * 输出：2
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点的数量在 [0, 10^4] 区间内。
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
+
+package main
+
+import "container/list"
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+    ans := 0
+    if root == nil {
+        return 0
+    }
+    queue := list.New()
+    queue.PushBack(root)
+    for queue.Len() > 0 {
+        length := queue.Len()
+        for i := 0; i < length; i++ {
+            node := queue.Remove(queue.Front()).(*TreeNode)
+            if node.Left != nil {
+                queue.PushBack(node.Left)
+            }
+            if node.Right != nil {
+                queue.PushBack(node.Right)
+            }
+        }
+        ans++//记录深度，其他的是层序遍历的板子
+    }
+    return ans
+}
+// @lc code=end
+
